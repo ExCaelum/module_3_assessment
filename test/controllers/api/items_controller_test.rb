c610fcf25fc3f1a972cb0b3b655db7c8aa98eba8
Parker P <exxcaelum@gmail.com>
2016-08-02 09:31:39 -0600

    the info is being returned correctly

--- test/controllers/api/items_controller_test.rb
+++ test/controllers/api/items_controller_test.rb
@@ -7,5 +7,8 @@ class Api::V1::ItemsControllerTest < ActionDispatch::IntegrationTest
     items = JSON.parse(response.body)
     assert_equal 2, items.count
     assert_equal "Hammer", items.last["name"]
+    assert_equal "A very good hammer", items.last["description"]
+    assert_equal "www.hammer.com", items.last["image_url"]
+    assert_equal nil, items.last["created_at"]
   end
 end
