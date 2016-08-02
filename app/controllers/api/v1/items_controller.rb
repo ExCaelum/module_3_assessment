f28161dadbfa7c2b777d9cf3b611c7d0f7d93b1e
Parker P <exxcaelum@gmail.com>
2016-08-02 09:52:06 -0600

    sorry for late commit, bio break, working on the delete method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,13 @@ class Api::V1::ItemsController < ApplicationController
     render json: Item.all
   end
 
+  def show
+    render json: Item.find(params["id"])
+  end
+
+  def destroy
+    Item.find(params["id"]).destroy
+    render :nothing => true, :status => 204
+  end
+
 end
--- test/controllers/api/items_controller_test.rb
+++ test/controllers/api/items_controller_test.rb
@@ -6,9 +6,27 @@ class Api::V1::ItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     items = JSON.parse(response.body)
     assert_equal 2, items.count
-    assert_equal "Hammer", items.last["name"]
-    assert_equal "A very good hammer", items.last["description"]
-    assert_equal "www.hammer.com", items.last["image_url"]
+    assert_equal "Hammer" || "wrench", items.first["name"]
+    assert_equal "A very good hammer" || "A very good wrench", items.first["description"]
+    assert_equal "www.hammer.com" || "www.wrench.com", items.first["image_url"]
     assert_equal nil, items.last["created_at"]
+  end
+
+  test "we receive one item" do
+    get "api/v1/items/1"
+    assert_response :success
+    item = JSON.parse(response.body)
+    assert_equal "Hammer", item["name"]
+    assert_equal "A very good hammer", item["description"]
+    assert_equal "www.hammer.com", item["image_url"]
+    assert_equal nil, item["created_at"]
+  end
+
+  test "we can delete an item" do
+    delete "api/v1/items/1"
+    assert_response :success
+
+    assert_equal nil, response.body
+    assert_equal 1, Item.all.count
   end
 end
